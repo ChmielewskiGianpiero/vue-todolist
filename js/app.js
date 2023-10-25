930c10e5a1e1c1a72334455bf5cb56226b42569f
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-10-25 13:45:53 +0200

    ADD - markup statico

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            title : "Vue To Do List"
+        }
+    },
+    methods: {
+        
+    }
+}).mount('#app');
